c9bf13fd70d3013decac8b342b78395516435861
Daniel López Rendón <155696710+danielLopezRendon@users.noreply.github.com>
2025-02-05 09:43:45 -0500

    se añaden control de transacciones

--- src/main/java/co/prueba/tecnica/module/client/service/ClientService.java
+++ src/main/java/co/prueba/tecnica/module/client/service/ClientService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
+import jakarta.transaction.Transactional;
 import co.prueba.tecnica.module.client.component.ClientComponent;
 import co.prueba.tecnica.module.client.dto.ClientDto;
 import co.prueba.tecnica.module.client.entity.Client;
@@ -19,20 +19,20 @@ public class ClientService extends ConvertObjects {
 	
 	@Autowired
 	private ClientComponent clientComponent;
-	
+	@Transactional
 	public EcosystemSuccessResponse<String> registrarCliente( ClientDto clientDto)throws EcosystemException {
 		return new EcosystemSuccessResponse<String>(null, clientComponent.registrarCliente(clientDto), HttpStatus.OK.value());
 	}
-	
+	@Transactional
 	public EcosystemSuccessResponse<String> actualizarCliente( ClientDto clientDto)throws EcosystemException {
 		
 		return new EcosystemSuccessResponse<String>(null,clientComponent.actualizarCliente(clientDto), HttpStatus.ACCEPTED.value());
 	}
-	
+	@Transactional
 	public ClientDto consultarCliente(Long identificacion) {
 		return returnDto(clientComponent.consultarCliente(identificacion), ClientDto.class);
 	}
-
+	@Transactional
 	public List<ClientDto> consultarClientesActivos() {
 		return returnListDto(clientComponent.consultarClientesActivos(), ClientDto.class);
 		
